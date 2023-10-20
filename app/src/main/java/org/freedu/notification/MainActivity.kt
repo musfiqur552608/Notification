ffd577aca9f75550be428bad941119c7cb14d466
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-10-21 01:10:23 +0600

    notification attributes

--- app/src/main/java/org/freedu/notification/MainActivity.kt
+++ app/src/main/java/org/freedu/notification/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.freedu.notification
 
 import android.app.NotificationManager
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
@@ -19,6 +20,10 @@ class MainActivity : AppCompatActivity() {
             notification.setContentText(binding.content.text.toString())
             notification.setSmallIcon(R.drawable.high)
             notification.setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.MAGENTA)
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
                 .build()
 
             val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
@@ -31,6 +36,10 @@ class MainActivity : AppCompatActivity() {
             notification.setContentText(binding.content.text.toString())
             notification.setSmallIcon(R.drawable.high)
             notification.setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.MAGENTA)
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
                 .build()
 
             val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
